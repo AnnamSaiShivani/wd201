8af412b150d994ec4beb4ff0ef105f6a7ac1239c
AnnamSaiShivani <115220329+AnnamSaiShivani@users.noreply.github.com>
2023-12-30 17:44:45 +0530

    Update todos.js

--- todo-app/__tests__/todos.js
+++ todo-app/__tests__/todos.js
@@ -72,16 +72,9 @@ describe("Todo Application", function () {
   });
 
   test("Deletes a todo with the given ID if it exists and sends a boolean response", async () => {
-     const response = await agent.post("/todos").send({
-       title: "Buy milk",
-       dueDate: new Date().toISOString(),
-       completed: false,
-     });
-     const parsedResponse = JSON.parse(response.text);
-     const todoID = parsedResponse.id;
-
-     const deleteResponse = await agent.delete(`/todos/${todoID}`);
-     const parsedDeleteResponse = JSON.parse(deleteResponse.text);
-     expect(parsedDeleteResponse).toBe(true);
-   });
+    // FILL IN YOUR CODE HERE
+    const res = await agent.delete("/todos/2");
+    const parsedResponse = JSON.parse(res.text);
+    expect(parsedResponse).toBe(true);
+  });
 });
